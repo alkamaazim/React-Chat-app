8714e42bd4ebda4c381a2d7353e2c347f1a4e776
alkama.azim <alkama.azim@ballisticlearning.com>
2023-07-24 15:22:37 +0530

    setup react-chat-app environment

---
+++ chat-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* <button className="btn">Button</button> */}
+      hello
+    </React.Fragment>
+  );
+}
+
+export default App;
